6cd11160deb2cf6874caa12bd1fd01af262aba71
void <flymi7@163.com>
2020-07-26 01:37:36 +0800

    快递系统添加IO功能

---
+++ src/com/kkb/chapter04/Demo10203001/utils/DeliveryException.java
@@ -0,0 +1,13 @@
+package com.kkb.chapter04.Demo10203001.utils;
+
+/**
+ * @Description: 异常类
+ * @Author: Du Junnan
+ * @Date: Created in 23:54 2020/7/17
+ * @Modified by:
+ */
+public class DeliveryException extends RuntimeException {
+    public DeliveryException(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/com/kkb/chapter04/Demo10203001/utils/OutOfSizeDeliveryException.java
@@ -0,0 +1,17 @@
+package com.kkb.chapter04.Demo10203001.utils;
+
+/**
+ * @Description:
+ * @Author: Du Junnan
+ * @Date: Created in 22:50 2020/7/17
+ * @Modified by:
+ */
+public class OutOfSizeDeliveryException extends RuntimeException {
+    public OutOfSizeDeliveryException() {
+        super("快递柜存储已经达到上限，请在快递柜空闲时进行操作");
+    }
+
+    public OutOfSizeDeliveryException(String message) {
+        super(message);
+    }
+}
